877bfd91318a690357514bd90047e1bcd0990c86
Attila Varga <srep.ati@gmail.com>
2021-01-20 12:23:27 +0100

    update Employee class

--- src/main/java/com/luv2code/springboot/thymeleafdemo/employeerelationshipmanager/entity/Employee.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/employeerelationshipmanager/entity/Employee.java
@@ -32,6 +32,13 @@ public class Employee {
 	
 	public Employee() {
 		
+	}	
+
+	public Employee(int id, String firstName, String lastName, String email) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
 	}
 
 	public Employee(String firstName, String lastName, String email) {
