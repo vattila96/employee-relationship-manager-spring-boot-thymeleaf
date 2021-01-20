a5fe9342f785b40bfd518e87caccdf0574cde3a5
Attila Varga <srep.ati@gmail.com>
2021-01-20 12:24:12 +0100

    update Employee controller

--- src/main/java/com/luv2code/springboot/thymeleafdemo/employeerelationshipmanager/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/employeerelationshipmanager/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.luv2code.springboot.thymeleafdemo.employeerelationshipmanager.model.Employee;
+import com.luv2code.springboot.thymeleafdemo.employeerelationshipmanager.entity.Employee;
 
 @Controller
 @RequestMapping("/employees")
