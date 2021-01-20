d2b539e50d152ecac3d6e937f7bbda99bbed1c75
Attila Varga <srep.ati@gmail.com>
2021-01-20 13:21:37 +0100

    add possibility to sort by name

--- src/main/java/com/luv2code/springboot/thymeleafdemo/employeerelationshipmanager/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/employeerelationshipmanager/dao/EmployeeRepository.java
@@ -1,6 +1,11 @@
 package com.luv2code.springboot.thymeleafdemo.employeerelationshipmanager.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.luv2code.springboot.thymeleafdemo.employeerelationshipmanager.entity.Employee;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> { }
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> { 
+	
+	public List<Employee> findAllByOrderByLastNameAsc();
+}
