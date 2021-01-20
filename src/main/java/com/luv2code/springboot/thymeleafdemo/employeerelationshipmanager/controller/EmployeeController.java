9b53ac7f8f7192acf83979c0a4d2b911492e3c94
Attila Varga <srep.ati@gmail.com>
2021-01-20 13:53:30 +0100

    add delete method

--- src/main/java/com/luv2code/springboot/thymeleafdemo/employeerelationshipmanager/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/employeerelationshipmanager/controller/EmployeeController.java
@@ -54,4 +54,11 @@ public class EmployeeController {
 		
 		return "redirect:/employees/list";
 	}
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int theId) {
+		employeeService.deleteById(theId);
+		
+		return "redirect:/employees/list";
+	}
 }
